c9a596b0377045472397cf4ca2662588926cbc47
Luiz Felipe Martins <lipihmartins@gmail.com>
2025-07-15 11:54:10 -0400

    add script

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+let content = document.querySelector(".content");
+let editbtn = document.querySelector(".edit-btn");
+let closebtn = document.querySelector(".popup__close-button");
+let savebtn = document.querySelector(".popup__save-button");
+let form = document.querySelector(".popup__form");
+
+function togglePopup() {
+  let popup = document.querySelector(".popup__edit-profile");
+  popup.classList.toggle("popup__opened");
+}
+
+editbtn.addEventListener("click", togglePopup);
+
+closebtn.addEventListener("click", togglePopup);
+
+function saveChanges(evt) {
+  evt.preventDefault();
+  let nameInput = document.querySelector(".popup__input_name");
+  let sobreMimInput = document.querySelector(".popup__input_sobre_mim");
+  let profileName = document.querySelector(".profile__name");
+  let profileDescription = document.querySelector(".profile__description");
+
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = sobreMimInput.value;
+
+  if (nameInput.value === "" || sobreMimInput.value === "") {
+    alert("Preencha todos os campos!");
+    return;
+  }
+  nameInput.value = "";
+  sobreMimInput.value = "";
+
+  togglePopup();
+}
+
+form.addEventListener("submit", saveChanges);
